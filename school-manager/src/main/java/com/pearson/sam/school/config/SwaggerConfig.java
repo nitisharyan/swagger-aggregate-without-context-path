4804ae4e7242a006288c00fe9363c4cdaec6566a
PEROOT\VKum985 <prince.kumar@pearson.com>
2018-07-19 18:58:34 +0530

    Removed unnecessary headings from swagger-ui

--- school-manager/src/main/java/com/pearson/sam/school/config/SwaggerConfig.java
+++ school-manager/src/main/java/com/pearson/sam/school/config/SwaggerConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -17,6 +18,14 @@ public class SwaggerConfig {
 	@Bean
 	public Docket productApi() {
 		return new Docket(DocumentationType.SWAGGER_2).select()
-				.apis(RequestHandlerSelectors.basePackage("com.pearson.sam.school")).paths(regex("/school.*")).build();
+				.apis(RequestHandlerSelectors.basePackage("com.pearson.sam.school")).paths(regex("/school.*")).build()
+				.apiInfo(metaInfo());
+	}
+
+	private ApiInfo metaInfo() {
+		@SuppressWarnings("deprecation")
+		ApiInfo apiInfo = new ApiInfo("API Documentation - School Manager", "", "", "", "", "", "");
+
+		return apiInfo;
 	}
 }
